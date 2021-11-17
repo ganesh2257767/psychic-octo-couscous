abaa61ad78d9cf426c0a077167337653f5d6cdce
Ganesh Kutty <ganesh.kutty@amdocs.com>
2021-11-17 16:30:02 +0530

    Added SMB support, Added radio buttons for additional corps, modified code to use a new format of master matrix.

--- EID check/Check EID Corp Ftax.py
+++ EID check/Check EID Corp Ftax.py
@@ -6,7 +6,8 @@ ico_path = getcwd()
 eid_df = DataFrame()
 
 def fmtcols(mylist, cols):
-    lines = (f"    |    ".join(mylist[i:i+cols]) for i in range(0,len(mylist),cols))
+    mylist = [f'{x:^40}' for x in mylist]
+    lines = (f'|'.join(mylist[i:i+cols]) for i in range(0, len(mylist), cols))
     return '\n\n'.join(lines)
 
 
@@ -14,12 +15,12 @@ def from_eid(eid):
     corp_ftax = []
     for i in master_df.index:
         if master_df[3][i] == eid.upper():
-            a = str(master_df[2][i]) + ' - ' + master_df[4][i].strip()
+            a = str(int(master_df[2][i])) + ' - ' + master_df[4][i].strip()
             corp_ftax.append(a)
 
     if corp_ftax:
         PopupScrolled(
-            f'Following corp/ftax combinations have EID: {eid.upper()}\n', fmtcols(corp_ftax, 5), size=(100, 20), title=f'{eid.upper()}')
+            f'Following corp/ftax combinations have EID: {eid.upper()}\n\n{fmtcols(corp_ftax, 5)}', size=(100, 20), title=f'{eid.upper()}')
 
     else:
         Popup(
@@ -29,30 +30,30 @@ def from_eid(eid):
 def get_corp_ftax(corp, offer_id, df):
     corpftax_altice = set()
     corpftax_legacy = set()
-    corpftax_altice_unltd = set()
-    corpftax_legacy_unltd = set()
+    smb = set()
+    
     offer_eid = {df[1][i] for i in df.index if df[2][i] == offer_id}
     for j in master_df.index:
         if master_df[1][j] in corp:
-            if master_df[5][j] == 'Y' and master_df[3][j] in offer_eid and master_df[6][j] == 'N':
+            if master_df[5][j] == 'Y' and master_df[3][j] in offer_eid:
                 corpftax_altice.add(
                     str(master_df[2][j])[:-2] + ' - ' + master_df[4][j].strip() + ' - ' + master_df[3][j].strip())
 
-            if master_df[5][j] == 'Y' and master_df[3][j] in offer_eid and master_df[6][j] == 'Y':
-                corpftax_altice_unltd.add(
-                     str(master_df[2][j])[:-2] + ' - ' + master_df[4][j].strip() + ' - ' + master_df[3][j].strip())
-
-            if master_df[5][j] == 'N' and master_df[3][j] in offer_eid and master_df[6][j] == 'N':
+            elif master_df[5][j] == 'N' and master_df[3][j] in offer_eid:
                 corpftax_legacy.add(
                      str(master_df[2][j])[:-2] + ' - ' + master_df[4][j].strip() + ' - ' + master_df[3][j].strip())
 
-            if master_df[5][j] == 'N' and master_df[3][j] in offer_eid and master_df[6][j] == 'Y':
-                corpftax_legacy_unltd.add(
-                    str(master_df[2][j])[:-2] + ' - ' + master_df[4][j].strip() + ' - ' + master_df[3][j].strip())
+            else:
+                smb.add(str(master_df[2][j])[:-2] + ' - ' + master_df[4][j].strip() + ' - ' + master_df[3][j].strip())
 
-    if corpftax_legacy or corpftax_altice or corpftax_altice_unltd or corpftax_legacy_unltd:
+
+    if corpftax_legacy or corpftax_altice:
         PopupScrolled(
-            f'Offer ID {offer_id} available in following corp/ftax combinations \n\nAltice One - Unlimited Combinations\n', fmtcols(sorted(corpftax_altice_unltd), 3), '\n\nAltice One - Limited Combinations\n', fmtcols(sorted(corpftax_altice), 3), '\n\nLegacy - Unlimited Combinations\n', fmtcols(sorted(corpftax_legacy_unltd), 3), '\n\nLegacy - Limited Combinations\n', fmtcols(sorted(corpftax_legacy), 3), size=(100, 20), title=f'{offer_id}')
+            f"Offer ID {offer_id} available in following corp/ftax combinations \n\nAltice One Combinations\n\n{fmtcols(sorted(corpftax_altice), 3)}\n\nLegacy Combinations\n\n{fmtcols(sorted(corpftax_legacy), 3)}", size=(100, 20), title=f'{offer_id}')
+    
+    elif smb:
+        PopupScrolled(f"<SMB> Offer ID {offer_id} available in following corp/ftax combinations \n\nCombinations for SMB\n\n{fmtcols(sorted(smb), 3)}", size=(100, 20), title=f'{offer_id}')
+
     else:
         Popup(
             f'Offer {offer_id} not available in {corp} or is invalid!\n\nPlease check offer ID or change corp and try again!', title='Error')
@@ -79,8 +80,11 @@ layout1 = [
 layout2 = [
     [Text(key='-SHEETNAME-', size=(50, 1))],
     [Text('Enter Offer ID'), InputText(key='-ID-')],
-    [Radio('QA 2', 'corp', key='-QA2-', default=True), Radio('QA INT',
-                                                                   'corp', key='-QAINT-'), Radio('Other corps', 'corp', key='-OTHERS-')],
+    [Radio('QA INT', 'corp', key='-QAINT-', default=True),
+     Radio('QA 1', 'corp', key='-QA1-'),
+     Radio('QA 2', 'corp', key='-QA2-'),
+     Radio('QA 3', 'corp', key='-QA3-'),
+     Radio('Other corps', 'corp', key='-OTHERS-')],
     [Button('Submit', key='-SUBMIT-'),
      Button('Upload another file', key='-ANOTHER-')]
 ]
@@ -96,7 +100,8 @@ layout = [
         Column(first_layout, key='-FCOL-', visible=False),
         Column(layout1, key='-COL1-', visible=False),
         Column(layout2, key='-COL2-', visible=False),
-        Column(layout3, key='-COL3-', visible=False)]
+        Column(layout3, key='-COL3-', visible=False)
+    ]
 ]
 
 
@@ -105,15 +110,15 @@ window = Window('Corp Ftax Combination checker', layout, resizable=True, icon=ic
 upload_flag = True
 while True:
     event, values = window.read()
+    print(event, values)
     if event in [WIN_CLOSED, 'Cancel']:
         break
 
     if event == '-UPLOAD0-' and values['-FILE0-']:
         path = values['-FILE0-']
-        columns = [0, 2, 3, 6, 13, 14]
-        colnames = [1, 2, 3, 4, 5, 6]
+        colnames = [1, 2, 3, 4, 5]
 
-        master_df = read_excel(path, usecols=columns, names=colnames)
+        master_df = read_excel(path, names=colnames)
         window['-COL0-'].update(visible=False)
         window['-FCOL-'].update(visible=True)
 
@@ -138,8 +143,12 @@ while True:
             corp = [7712, 7709]
         elif values['-QAINT-']:
             corp = [7702, 7704, 7710, 7715]
+        elif values['-QA1-']:
+            corp = [7708, 7711]
+        elif values['-QA3-']:
+            corp = [7707, 7714]
         else:
-            corp = [7701, 7703, 7705, 7706, 7707, 7708, 7711, 7713, 7714]
+            corp = [7701, 7703, 7705, 7706, 7713]
 
         try:
             offer_id = int(offer_id)
